e9efd884d828596305b4bd6f3f49330c91fb26d1
KayKay14 <107590057+KayKay14@users.noreply.github.com>
2023-04-05 08:19:35 -0400

    commit message

--- login.php
+++ login.php
@@ -1,76 +1,23 @@
-
-
 <?php
-    echo"<h1>runnig login php</h1>";
-  /*
-    $email = $_POST['email'];  
-    $password = $_POST['password'];  
-    $conn = mysqli_connect('localhost', 'root', '', 'BusEZdb');
+    include("connection.php");
+    include("signup.php");
 
+    if(isset($POST['submit'])){
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
-    $email = stripcslashes($email);  
-        $password = stripcslashes($password);  
-        $email = mysqli_real_escape_string($conn, $email);  
-        $password = mysqli_real_escape_string($conn, $password);  
+        $sql = "select * from user where email = '$email' and password= '$password'";
+        $result = mysqli_query($conn, $sqli);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $count = mysqli_num_rows($result);
+        if($count==1){
+            header("Location:index.php");
+        }else{
+            echo '<script> window.location.href = "index.php"; alert("Login Failed. Invalid email or password")
+            </script>';
+        }
+    }
 
-
-        $sql = "select *from signup where email = '$email' and password = '$password'";  
-        $result = mysqli_query($conn, $sql);  
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
-        $count = mysqli_num_rows($result);  
-   
-        if($count == 1){  
-            echo "<h1><center> Login successful </center></h1>";  
-        }  
-        else{  
-            echo "<h1> Login failed. Invalid email or password.</h1>";  
-        }  
-        
-        */
+    
 ?>  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-*/
-
-   /* $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    //connection
-    $con = new mysqli("host","localhost", "root", " ", "application database");
-    if($con->connect_error){
-        die("Failed to connect : ".$con->connect_error);
-    
-    }else{
-        $stmt = $con->prepare("select* from signup where email = ?");
-        $stmt->bind_param("s", $email);
-        $stmt->execute();
-        $stmt_result = -> $stmt->get_result();
-        if($stmt_result->num_rows > 0){
-            $data = $stmt_result->fetch_assoc();
-            if($data['password']=== $password){
-                echo "Logged in Sucessfully!!!"
-            }else{
-                echo "Invalid email or password"
-            }
-
-        }else {
-            echo "Invalid email or password"
-        }
-    }*/
-
-?>
