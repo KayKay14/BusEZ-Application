f4fe3cf3e6cd06bafc76b973a21df04da6487a8d
KayKay14 <107590057+KayKay14@users.noreply.github.com>
2023-04-04 22:38:25 -0400

    Commit message

---
+++ signupandlogin/connection.php
@@ -0,0 +1,19 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = " ";
+    $db_name = "busezdb";
+
+    //create connection
+    $conn = new mysqli($servername, $username, $password, $db_name);
+
+    //check connection
+
+    if($conn->connect_error){
+        die("Connection failed ".$conn->connect_error);
+    }
+    echo "Connected successfully";
+
+    mysqli_close($conn);
+?>
